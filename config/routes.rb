34c966bbe1e0cb8f8edab0e0db01d547f6c6150e
Amadou Ibrahim <amadou140589@gmail.com>
2020-02-17 22:41:08 +0100

    Session controller and new session form added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'sign_out', to: 'sessions#destroy'
 end
