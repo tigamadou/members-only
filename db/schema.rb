68413bfa2803bde843e0479f20245c1688d08400
Amadou Ibrahim <amadou140589@gmail.com>
2020-02-17 23:12:50 +0100

    remember token field added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200217193752) do
+ActiveRecord::Schema.define(version: 20200217220250) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20200217193752) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_token"
   end
 
 end
