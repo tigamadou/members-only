e90f362f12cfd0f941a264f2a4b9e72a82cf19c1
Treasure Kabareebe <treasurecalm@gmail.com>
2020-02-22 00:12:42 +0300

    Add sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'sign_in', action:'new', controller: 'sessions'
   post 'sign_in', action:'create', controller:'sessions'
   delete 'sign_out', action: 'destroy', controller: 'sessions'
-
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  resources :user
   # Post Routes
 
   resources :posts
