fde8f63aef4c436782700cecf75d7b9cd54d0683
Treasure Kabareebe <treasurecalm@gmail.com>
2020-02-22 03:32:44 +0300

    Add setter method for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find_by(remember_token: cookies.permanent[:remember_token]) if session[:user_id]
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
   def sign_in(user)
     session[:user_id] = user.id
     user.update_token
     cookies.permanent[:remember_token] = user.remember_token
-    @current_user = user
+    current_user=(user)
   end
 
   def sign_out
     @current_user = nil
     cookies.permanent.delete(:remember_token)
     session.delete(:user_id)
-  end
-
-  def current_user
-    @current_user ||= User.find_by(remember_token: cookies.permanent[:remember_token]) if session[:user_id]
   end
 
   def signed_in?
